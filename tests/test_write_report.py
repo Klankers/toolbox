1b0fac7c001dc4bffc54f47ba75a56e7af6694a1
Aaron Mau <aaronmau40@gmail.com>
2026-02-20 17:10:55 +0100

    Ruff

--- tests/test_write_report.py
+++ tests/test_write_report.py
@@ -9,43 +9,51 @@ from datetime import datetime, timezone
 from unittest.mock import patch
 
 
-
-
 def test_flatten_qc_dict():
     qc_dict = {
-        "temperature": {
+        "TEMP_QC": {
             "range_test": {
                 "stats": {"min": 0, "max": 30},
                 "flag_counts": {
-                    0:0, 1:2212921, 2:0, 3:0,
-                    4:0, 5:0, 6:0, 7:0, 8:0, 9:0},
+                    0: 0,
+                    1: 2212921,
+                    2: 0,
+                    3: 2500,
+                    4: 0,
+                    5: 0,
+                    6: 0,
+                    7: 0,
+                    8: 0,
+                    9: 0,
+                },
             }
         },
-        "salinity": {},  # should be skipped entirely
+        "CNDC_QC": {},  # should be skipped entirely
     }
 
     result = write_report.flatten_qc_dict(qc_dict)
 
     expected = [
-        ["temperature", "range_test", 1, "2212921"],
-        ["temperature", "range_test", 3, "0"],
+        ["TEMP_QC", "range_test", 1, "2,212,921"],
+        ["TEMP_QC", "range_test", 3, "2,500"],
     ]
 
     assert result == expected
-    assert "salinity" not in [item for sublist in expected for item in sublist]
+    assert "CNDC_QC" not in [item for sublist in expected for item in sublist]
+
 
 def test_current_info():
-
     #   Better test - uses mock to emulate OS with context managers
     #   import getpass becomes import write_report.getpass within mock
     fixed_now = datetime(2026, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
-    with patch.object(write_report, "datetime") as mock_datetime, \
-        patch.object(write_report.getpass, "getuser", return_value="aaron-mau"), \
-        patch.object(write_report, "version", return_value="0.1.dev318+gdaacfb0d8"), \
-        patch.object(write_report.platform, "python_version", return_value="3.14.2"), \
-        patch.object(write_report.platform, "system", return_value="Linux"), \
-        patch.object(write_report.platform, "release", return_value="6.17.0-8-generic"):
-
+    with (
+        patch.object(write_report, "datetime") as mock_datetime,
+        patch.object(write_report.getpass, "getuser", return_value="aaron-mau"),
+        patch.object(write_report, "version", return_value="0.1.dev318+gdaacfb0d8"),
+        patch.object(write_report.platform, "python_version", return_value="3.14.2"),
+        patch.object(write_report.platform, "system", return_value="Linux"),
+        patch.object(write_report.platform, "release", return_value="6.17.0-8-generic"),
+    ):
         mock_datetime.now.return_value = fixed_now
         result = write_report.current_info()
 
@@ -58,5 +66,3 @@ def test_current_info():
     }
 
     assert result == expected
-
-
